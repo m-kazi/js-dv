d0631c9b5329226f41288dd6a66f80f4b1f3814a
m-kazi <kazzcode@gmail.com>
2024-03-06 16:01:37 +0600

    Refactoring RPS

--- first-game/rps.js
+++ first-game/rps.js
@@ -1,45 +1,43 @@
-// Rock, Paper, Scissors Interactive Game
-
-let playGame = confirm("Shall we play?");
-
+// Rock, Paper, Scissors: Refactored with While Loop and an Array
+let playGame = confirm("Shall we play rock, paper, or scissors?");
 if (playGame) {
-    // play
-    let playerChoice = prompt("Please enter rock, paper, or scissors.");
-    if (playerChoice) {
-        let playerOne = playerChoice.trim().toLowerCase();
-        if (
-            playerOne === "rock" ||
-            playerOne === "paper" ||
-            playerOne === "scissors"
-        ) {
-            let computerChoice = Math.floor(Math.random() * 3 + 1);
-            let computer =
-                computerChoice === 1
-                    ? "rock"
-                    : computerChoice === 2
-                    ? "paper"
-                    : "scissors";
-            // comparing & final result
-            let result =
-                playerOne === computer
-                    ? "Tie"
-                    : playerOne === "rock" && computer === "paper"
-                    ? `playerOne: ${playerOne}\ncomputer: ${computer}\ncomputer wins`
-                    : playerOne === "paper" && computer === "scissors"
-                    ? `playerOne: ${playerOne}\ncomputer: ${computer}\ncomputer wins`
-                    : playerOne === "scissors" && computer === "rock"
-                    ? `playerOne: ${playerOne}\ncomputer: ${computer}\ncomputer wins`
-                    : "PlayerOne wins";
-            alert(result);
+    //play
+    while (playGame) {
+        const playerChoice = prompt("Please enter rock, paper, or scissors.");
+        if (playerChoice || playerChoice === "") {
+            const playerOne = playerChoice.trim().toLowerCase();
+            if (
+                playerOne === "rock" ||
+                playerOne === "paper" ||
+                playerOne === "scissors"
+            ) {
+                const computerChoice = Math.floor(Math.random() * 3);
+                const rpsArray = ["rock", "paper", "scissors"];
+                const computer = rpsArray[computerChoice];
 
-            let playAgain = confirm("Play Again!");
-            playAgain ? location.reload() : alert("OK, thanks for playing.");
+                const result =
+                    playerOne === computer
+                        ? "Tie game!"
+                        : playerOne === "rock" && computer === "paper"
+                        ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
+                        : playerOne === "paper" && computer === "scissors"
+                        ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
+                        : playerOne === "scissors" && computer === "rock"
+                        ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
+                        : `playerOne: ${playerOne}\nComputer: ${computer}\nplayerOne wins!`;
+                alert(result);
+                playGame = confirm("Play Again?");
+                if (!playGame) alert("Ok, thanks for playing.");
+                continue;
+            } else {
+                alert("You didn't enter rock, paper, or scissors.");
+                continue;
+            }
         } else {
-            alert("You didn't enter rock, paper, or scissors.");
+            alert("I guess you changed your mind. Maybe next time.");
+            break;
         }
-    } else {
-        alert("I guess you changed your mind, may be next time!");
     }
 } else {
-    alert("Ok, maybe next time!");
+    alert("Ok, maybe next time.");
 }
